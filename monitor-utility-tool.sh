7177782370e46e788692dade859f3a7002b9505d
nocturnalismee <maarifefriyan@gmail.com>
2025-04-19 15:54:28 +0700

    add comand scan backdoor by pattern or string

--- monitor-utility-tool.sh
+++ monitor-utility-tool.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Arief Project monitoring
 
-# Opsi Warna Bash
+# Additional color codes for theming
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-# Define additional color codes for theming
 BOLD='\033[1m'
 UNDERLINE='\033[4m'
 HEADER_BG='\033[44m'
@@ -38,7 +37,7 @@ print_centered_in_box() {
 # Function to display a themed header
 print_header() {
     clear
-    print_centered_in_box "Server Admin Utility Tool"
+    print_centered_in_box "Monitoring Commandline Utility"
     echo
 }
 
@@ -48,6 +47,7 @@ print_footer() {
     echo
 }
 
+# Disk Audit Functions
 # Function to find files larger than 1GB, sorted by user then size (desc)
 find_large_files() {
     print_header
@@ -113,6 +113,75 @@ check_email_usage() {
     read -p "Press Enter to return to the menu..."
 }
 
+# Function to find and optionally delete files of size 0 Kb
+find_zero_size_files() {
+    print_header
+    echo -e "${YELLOW}Mencari file dengan ukuran 0 Kb di /home/*...${NC}"
+    echo -e "${YELLOW}Mohon menunggu...${NC}"
+    echo "-----------------------------------------"
+    local results
+    # Use find to locate files of size 0 Kb
+    results=$(find /home/* -type f -size 0 2>/dev/null)
+    if [ -z "$results" ]; then
+        echo "Tidak ada file dengan ukuran 0 Kb ditemukan."
+    else
+        echo -e "${RED}File dengan ukuran 0 Kb ditemukan:${NC}"
+        echo "Ukuran | File"
+        echo "-------------------------"
+        echo "$results" | while read -r file; do
+            size=$(du -sh "$file" | awk '{print $1}')
+            printf "%-7s | %s\n" "$size" "$file"
+        done
+        echo "-------------------------"
+        read -p "Apakah Anda ingin menghapus file-file ini? (y/n): " choice
+        if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+            echo "$results" | xargs rm -f
+            echo -e "${GREEN}File-file telah dihapus.${NC}"
+        else
+            echo -e "${YELLOW}Penghapusan dibatalkan.${NC}"
+        fi
+    fi
+    echo "-----------------------------------------"
+    echo -e "${GREEN}Pencarian selesai.${NC}"
+    print_footer
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
+# DDoS Mitigation Functions
+# Function to track DDoS attacks by number of connections per IP
+track_ddos_by_ip() {
+    print_header
+    echo -e "${YELLOW}Melacak serangan DDoS berdasarkan jumlah koneksi per IP...${NC}"
+    echo "-----------------------------------------"
+    netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head
+    echo "-----------------------------------------"
+    print_footer
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
+# Function to check number of httpd connections per IP
+check_httpd_connections() {
+    print_header
+    echo -e "${YELLOW}Memeriksa jumlah koneksi httpd per IP...${NC}"
+    echo "-----------------------------------------"
+    netstat -anp | grep ':80' | grep ESTABLISHED | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head
+    echo "-----------------------------------------"
+    print_footer
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
+# Function to check httpd processes with SYN_RECV status
+check_syn_recv() {
+    print_header
+    echo -e "${YELLOW}Memeriksa proses httpd dengan status SYN_RECV...${NC}"
+    echo "-----------------------------------------"
+    netstat -n | grep 'SYN_RECV' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head
+    echo "-----------------------------------------"
+    print_footer
+    read -p "Tekan Enter untuk kembali ke menu..."
+}
+
+# Judol Audit Functions
 # Function to search for judi scripts using local keyword file
 find_judi_scripts() {
     print_header
@@ -165,36 +234,70 @@ find_judi_scripts() {
     read -p "Press Enter to return to the menu..."
 }
 
-# Function to find and optionally delete files of size 0 Kb
-find_zero_size_files() {
-    print_header
-    echo -e "${YELLOW}Mencari file dengan ukuran 0 Kb di /home/*...${NC}"
-    echo -e "${YELLOW}Mohon menunggu...${NC}"
-    echo "-----------------------------------------"
-    local results
-    # Use find to locate files of size 0 Kb
-    results=$(find /home/* -type f -size 0 2>/dev/null)
-    if [ -z "$results" ]; then
-        echo "Tidak ada file dengan ukuran 0 Kb ditemukan."
-    else
-        echo -e "${RED}File dengan ukuran 0 Kb ditemukan:${NC}"
-        echo "Ukuran | File"
-        echo "-------------------------"
-        echo "$results" | while read -r file; do
-            size=$(du -sh "$file" | awk '{print $1}')
-            printf "%-7s | %s\n" "$size" "$file"
-        done
-        echo "-------------------------"
-        read -p "Apakah Anda ingin menghapus file-file ini? (y/n): " choice
-        if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-            echo "$results" | xargs rm -f
-            echo -e "${GREEN}File-file telah dihapus.${NC}"
-        else
-            echo -e "${YELLOW}Penghapusan dibatalkan.${NC}"
-        fi
+# BACKDOOR SCAN FUNCTION
+# Fungsi untuk meminta input username cPanel
+get_username() {
+    read -p "Input username cPanel: " USERNAME
+    if [[ -z "$USERNAME" ]]; then
+        echo "Username tidak boleh kosong."
+        exit 1
     fi
-    echo "-----------------------------------------"
-    echo -e "${GREEN}Pencarian selesai.${NC}"
+}
+
+# Fungsi untuk memeriksa keberadaan direktori website
+check_website_dir() {
+    WEBSITE_DIR="/home/$USERNAME/"
+    if [[ ! -d "$WEBSITE_DIR" ]]; then
+        echo "Direktori $WEBSITE_DIR tidak ditemukan."
+        exit 1
+    fi
+}
+
+# Fungsi untuk melakukan scanning backdoor
+perform_scan() {
+    local patterns=("upload_chunk_size_bytes" "fm_redirect" "auth_users" "getClientIP")
+    found_any=false
+    echo "SCANNING, PLEASE WAIT !!"
+    local dots=""
+    while [ -d /proc/$$ ]; do
+        printf "\r[%-5s]" "$dots"
+        sleep 0.5
+        dots="${dots}."
+        if [ "${#dots}" -gt 5 ]; then
+            dots=""
+        fi
+    done &
+    loading_pid=$!
+
+    for pattern in "${patterns[@]}"; do
+        find "$WEBSITE_DIR" -type f | while read -r local_file; do
+            if grep -qF "$pattern" "$local_file"; then
+                if [ "$found_any" = false ]; then
+                    echo "Potensi script backdoor ditemukan pada path direktori dibawah:"
+                    found_any=true
+                fi
+                echo "$local_file"
+            fi
+        done
+    done
+
+    echo
+    if [ "$found_any" = false ]; then
+        echo "Tidak ada potensi script backdoor yang ditemukan saat ini."
+    fi
+
+    kill $loading_pid
+    wait $loading_pid 2>/dev/null
+    printf "\r          \r"
+    echo "Pencarian selesai."
+}
+
+# Fungsi untuk menampilkan menu scan backdoor
+show_backdoor_scan_menu() {
+    print_header
+    get_username
+    check_website_dir
+    perform_scan
     print_footer
     read -p "Tekan Enter untuk kembali ke menu..."
 }
@@ -208,123 +311,6 @@ validate_input() {
     return 0
 }
 
-# Function to track DDoS attacks by number of connections per IP
-track_ddos_by_ip() {
-    print_header
-    echo -e "${YELLOW}Melacak serangan DDoS berdasarkan jumlah koneksi per IP...${NC}"
-    echo "-----------------------------------------"
-    netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head
-    echo "-----------------------------------------"
-    print_footer
-    read -p "Tekan Enter untuk kembali ke menu..."
-}
-
-# Function to check number of httpd connections per IP
-check_httpd_connections() {
-    print_header
-    echo -e "${YELLOW}Memeriksa jumlah koneksi httpd per IP...${NC}"
-    echo "-----------------------------------------"
-    netstat -anp | grep ':80' | grep ESTABLISHED | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head
-    echo "-----------------------------------------"
-    print_footer
-    read -p "Tekan Enter untuk kembali ke menu..."
-}
-
-# Function to check httpd processes with SYN_RECV status
-check_syn_recv() {
-    print_header
-    echo -e "${YELLOW}Memeriksa proses httpd dengan status SYN_RECV...${NC}"
-    echo "-----------------------------------------"
-    netstat -n | grep 'SYN_RECV' | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head
-    echo "-----------------------------------------"
-    print_footer
-    read -p "Tekan Enter untuk kembali ke menu..."
-}
-
-# Function to display the DDoS mitigation menu
-show_ddos_menu() {
-    while true; do
-        print_header
-        echo "Mitigasi DDOS Menu:"
-        echo
-        options=(
-            "Track DDoS by IP"
-            "Check httpd Connections"
-            "Check SYN_RECV Status"
-            "Kembali ke Menu Utama"
-        )
-        select opt in "${options[@]}"
-        do
-            validate_input "$REPLY" || { sleep 1; continue; }
-            case $REPLY in
-                1)
-                    track_ddos_by_ip
-                    ;;
-                2)
-                    check_httpd_connections
-                    ;;
-                3)
-                    check_syn_recv
-                    ;;
-                4)
-                    return
-                    ;;
-                *)
-                    echo -e "${RED}Invalid option $REPLY. Please try again.${NC}"
-                    sleep 1
-                    continue
-                    ;;
-            esac
-        done
-    done
-}
-
-# Function to display the Disk Audit menu
-show_disk_audit_menu() {
-    while true; do
-        print_header
-        echo "Audit Disk Menu:"
-        echo
-        options=(
-            "Audit File Backup/Arsip > 1GB"
-            "Audit Disk Usage Mail"
-            "Audit & Delete File 0 Kb"
-            "Kembali ke Menu Utama"
-        )
-        select opt in "${options[@]}"
-        do
-            validate_input "$REPLY" || { sleep 1; continue; }
-            case $REPLY in
-                1)
-                    find_large_files
-                    ;;
-                2)
-                    check_email_usage
-                    ;;
-                3)
-                    find_zero_size_files
-                    ;;
-                4)
-                    return
-                    ;;
-                *)
-                    echo -e "${RED}Invalid option $REPLY. Please try again.${NC}"
-                    sleep 1
-                    continue
-                    ;;
-            esac
-        done
-    done
-}
-
-# Function to display the Judol Audit menu
-show_judol_audit_menu() {
-    print_header
-    echo "Audit Judol Menu:"
-    echo
-    find_judi_scripts || true
-}
-
 # Menu Utama Bash
 while true; do
     print_header
@@ -334,7 +320,8 @@ while true; do
         "Audit Disk"
         "Audit Judol"
         "Mitigasi DDoS"
-        "Keluar"
+        "Mitigasi Backdoor (beta)"
+        "EXIT"
     )
     select opt in "${options[@]}"
     do
@@ -350,6 +337,9 @@ while true; do
                 show_ddos_menu
                 ;;
             4)
+                show_backdoor_scan_menu
+                ;;
+            5)
                 echo "Exiting."
                 exit 0
                 ;;
@@ -360,12 +350,11 @@ while true; do
                 ;;
         esac
     done
+    # Ensure keyword file has correct permissions
+    if [ -r "$keyword_file" ]; then
+        chmod 600 "$keyword_file"
+    fi
+
+    # Use mktemp for temporary files if needed
+    temp_file=$(mktemp)
 done
-
-# Ensure keyword file has correct permissions
-if [ -r "$keyword_file" ]; then
-    chmod 600 "$keyword_file"
-fi
-
-# Use mktemp for temporary files if needed
-temp_file=$(mktemp)
